ea16cba28d4e9eb2e510243fe7d5fb933575082b
元皓yh <157942818+hpware@users.noreply.github.com>
2025-01-04 11:08:41 +0800

    Create remove-users.ps1

---
+++ windows/remove-users.ps1
@@ -0,0 +1,8 @@
+$path="CN=Users, DC=ad,DC=com" # 取代 ad 和 com 成 你的AD網域
+$domain="ad.com" # AD 網域
+$user="AD" # 使用者開頭
+$count=1..10 #多少？ 
+foreach($i in $count) {
+	$num=$i.ToString('00')
+	Remove-AdUser -Identity "$user$num" -Confirm:$false
+}
